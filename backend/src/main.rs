a83e4599865916cf2662ab0dfd3a805669cd073a
PiasekDev <63462890+PiasekDev@users.noreply.github.com>
2026-04-08 23:46:58 +0200

    Do the axum hello world

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .expect("the address and port should be available");
+
+    axum::serve(listener, app)
+        .await
+        .expect("the server should start successfully");
 }
