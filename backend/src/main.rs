e0bc6a21830e62d3f733e5ecd28196d4cc186806
PiasekDev <63462890+PiasekDev@users.noreply.github.com>
2026-04-09 00:55:20 +0200

    Fix the indentation

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -6,30 +6,30 @@ use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitEx
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    color_eyre::install()?;
-    dotenvy::dotenv().ok();
-    tracing_subscriber::registry()
-        .with(tracing_subscriber::fmt::layer())
-        .with(EnvFilter::from_default_env())
-        .init();
+	color_eyre::install()?;
+	dotenvy::dotenv().ok();
+	tracing_subscriber::registry()
+		.with(tracing_subscriber::fmt::layer())
+		.with(EnvFilter::from_default_env())
+		.init();
 
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL should be set");
-    let bind_address = env::var("BIND_ADDRESS").expect("BIND_ADDRESS should be set");
+	let database_url = env::var("DATABASE_URL").expect("DATABASE_URL should be set");
+	let bind_address = env::var("BIND_ADDRESS").expect("BIND_ADDRESS should be set");
 
-    let db_pool = PgPoolOptions::new()
-        .max_connections(10)
-        .connect(&database_url)
-        .await?;
+	let db_pool = PgPoolOptions::new()
+		.max_connections(10)
+		.connect(&database_url)
+		.await?;
 
-    sqlx::migrate!("../migrations").run(&db_pool).await?;
+	sqlx::migrate!("../migrations").run(&db_pool).await?;
 
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+	let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
-    let listener = tokio::net::TcpListener::bind(&bind_address).await?;
+	let listener = tokio::net::TcpListener::bind(&bind_address).await?;
 
-    tracing::info!("listening on {bind_address}");
+	tracing::info!("listening on {bind_address}");
 
-    axum::serve(listener, app).await?;
+	axum::serve(listener, app).await?;
 
-    Ok(())
+	Ok(())
 }
